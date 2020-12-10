f798fc5a8b16ac0508b062e37b5a4bad78659da3
Ally Thach <allythach379@gmail.com>
2020-12-10 15:35:50 -0800

    .h rename

--- source/main.c
+++ source/main.c
@@ -13,7 +13,7 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #include "timer.h"
-#include "usart.h"
+#include "usart_ATmega1284.h"
 #endif
 
 
@@ -23,43 +23,44 @@ enum U_States {U_Init, U_On, U_Off} U_State;
 void LED_Output(){
     switch(U_State){
         case U_Init:
-            output = 0x01;
+            output = USART_Receive(0);
             UDR = output;
+            USART_Flush();
             U_State = U_On;
             break;
         case U_On:
-            if (USART_HasTransmitted(0)){
+            /*if (USART_HasTransmitted(0)){
                 USART_Flush(0);
             }
             output = 0x01;
             UDR = output;
             if (USART_IsSendReady(0)){
                 USART_Send(output, 0);
-            }
-            if (USART_HasTransmitted(0)){
+            }*/
+            if (USART_HasReceived(0)){
                 U_State = U_Off;
             } else {
                 U_State = U_On;
             }
             break;
         case U_Off:
-            if (USART_HasTransmitted(0)){
+            /*if (USART_HasTransmitted(0)){
                                 USART_Flush(0);
-                        }
-                        output = 0x01;
-                        UDR = output;
-                        if (USART_IsSendReady(0)){
-                                USART_Send(output, 0);
-                        }
-                        if (USART_HasTransmitted(0)){
-                                U_State = U_On;
-                        } else {
-                                U_State = U_Off;
-                        }
+            }
+            output = 0x01;
+            UDR = output;
+            if (USART_IsSendReady(0)){
+                USART_Send(output, 0);
+            }*/
+            if (USART_HasReceived(0)){
+                U_State = U_On;
+            } else {
+                U_State = U_Off;
+            }
             break;
     }
 }
-//Leader Code
+//Follower Code
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0xFF; PORTA = 0x00;
