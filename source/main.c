3385b0e58f91dbf221e0c0d962929995b9b78bb7
Ally Thach <allythach379@gmail.com>
2020-12-10 15:29:59 -0800

    change usart.h name

--- source/main.c
+++ source/main.c
@@ -1,23 +1,76 @@
-/*	Author: lab
+/*	Author: Ally Thach, athac007
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Lab Section: 24
+ *	Assignment: Lab 14  Exercise 1
+ *	Exercise Description: Two microcontrollers each have an LED connected to PA0. Both LEDs are synchronously toggled on/off in one second intervals using USART.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *  DEMO LINK:
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "timer.h"
+#include "usart.h"
 #endif
 
+
+unsigned char output;
+enum U_States {U_Init, U_On, U_Off} U_State;
+
+void LED_Output(){
+    switch(U_State){
+        case U_Init:
+            output = 0x01;
+            UDR = output;
+            U_State = U_On;
+            break;
+        case U_On:
+            if (USART_HasTransmitted(0)){
+                USART_Flush(0);
+            }
+            output = 0x01;
+            UDR = output;
+            if (USART_IsSendReady(0)){
+                USART_Send(output, 0);
+            }
+            if (USART_HasTransmitted(0)){
+                U_State = U_Off;
+            } else {
+                U_State = U_On;
+            }
+            break;
+        case U_Off:
+            if (USART_HasTransmitted(0)){
+                                USART_Flush(0);
+                        }
+                        output = 0x01;
+                        UDR = output;
+                        if (USART_IsSendReady(0)){
+                                USART_Send(output, 0);
+                        }
+                        if (USART_HasTransmitted(0)){
+                                U_State = U_On;
+                        } else {
+                                U_State = U_Off;
+                        }
+            break;
+    }
+}
+//Leader Code
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRA = 0xFF; PORTA = 0x00;
+    DDRD = 0x00; PORTD = 0xFF;
     /* Insert your solution below */
+    TimerOn();
+    TimerSet(100);
+    initUSART(0);
     while (1) {
-
+    LED_Output();
+    PORTA = output;
     }
     return 1;
 }
