ed12fba457aa9577a6fa0a18206bc06315afa9d5
Ally Thach <allythach379@gmail.com>
2020-12-12 20:16:42 -0800

    w

--- source/main.c
+++ source/main.c
@@ -12,38 +12,42 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#include "timer.h"
-#include "usart_ATmega1284.h"
 #endif
+#include "usart_ATmega1284.h"
+#include "Timer.h"
+unsigned char temp = 0x00;
 
-unsigned char output = 0x00;
 
-void receive()
-{
-    if(USART_HasReceived(0))
-    {
-        output = USART_Receive(0);
-        PORTA = output;
+
+void receiveTick(){
+    
+    if(USART_HasReceived(0)){
+        temp = USART_Receive(0);
+        PORTA = temp;
         USART_Flush(0);
+        
     }
+    
 }
 
-//Follower Code
+
+
+
+
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0xFF; PORTA = 0x00;
-    //DDRD = 0x00; PORTD = 0xFF;
-    /* Insert your solution below */
-    TimerOn();
-    TimerSet(1000);
+
     initUSART(0);
-    PORTA = output;
+    TimerSet(5);
+    TimerOn();
+    PORTA = temp;
+    USART_Flush(0);
+    /* Insert your solution below */
     while (1) {
-        while (!TimerFlag) {
-            
-        }
-        TimerFlag = 0;
-        receiveTick();
+    while(!TimerFlag);
+    TimerFlag = 0;
+    receiveTick();
     }
     return 1;
 }
