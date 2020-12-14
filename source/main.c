01209372bd060e7844522320d3c7ebf62198684e
Ally Thach <allythach379@gmail.com>
2020-12-13 17:54:35 -0800

    pt3

--- source/main.c
+++ source/main.c
@@ -1,59 +1,100 @@
 /*	Author: Ally Thach, athac007
  *  Partner(s) Name: 
  *	Lab Section: 24
- *	Assignment: Lab 14  Exercise 1
- *	Exercise Description: Two microcontrollers each have an LED connected to PA0. Both LEDs are synchronously toggled on/off in one second intervals using USART.
- *
+ *	Assignment: Lab 14  Exercise 3
+ *	Exercise Description: Expand on exercise 2 by replacing "manual" mode switching with "automatic" mode switching.  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
  *  DEMO LINK:
  */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#include "timer.h"
-#include "usart_ATmega1284.h"
-#endif
-
 unsigned char tmpA = 0x00;
-unsigned char toggle;
+unsigned char tickCount = 100;
+unsigned char receiveCount = 5;
+unsigned char transmitCount = 200;
+unsigned char currCount = 5;
+unsigned char leadLED = 0x00;
+unsigned int cntr = 0;
 
-void sendTick()
+enum States{start, leader, follower} currState;
+void Tick()
 {
-    if(USART_IsSendReady(0))
+    switch(currState)
     {
-        PORTA = tmpA;
-        USART_Send(tmpA, 0);
-        while(!USART_HasTransmitted(0))
-        {
-            continue;
-        }
 
-        if(tmpA == 0x01)
-        {
-            tmpA = 0x00;
-        }
-        else
-        {
-            tmpA = 0x01;
-        }
-        
+        case start:
+            currCount = receiveCount;
+            currState = Follow;
+            break;
+
+        case leader:
+            cntr = 0;
+            leadLED = 1;
+            if(!USART_HasReceived(0))
+            {
+                currState = leader;
+                currCount = transmitCount;
+                if(USART_IsSendReady(1))
+                {
+                    PORTA = tmpA;
+                    USART_Send(tmpA, 1);
+                    while(!USART_HasTransmitted(1))
+                    {
+                        continue;
+                    }
+
+                    if(tmpA == 0x01){
+                        tmpA = 0x00;
+                    }
+                    else
+                    {
+                        tmpA = 0x01;
+                    }
+                }
+            }
+            else
+            {
+                tmpA = USART_Receive(0);
+                PORTA = tmpA;
+                USART_Flush(0);
+                currState = follower;
+                cntr = 0;
+                if(USART_IsSendReady(1))
+                {
+                    tmpA = 0;
+                    PORTA = tmpA;
+                    USART_Send(0, 1);
+                    while(!USART_HasTransmitted(1))
+                    {
+                        continue;
+                    }
+                }
+                USART_Flush(0);
+            }
+            break;
+
+        case follower:
+            leadLED = 0;
+            currCount = receiveCount;
+            if(USART_HasReceived(0))
+            {
+                tmpA = USART_Receive(0);
+                PORTA = tmpA;
+                USART_Flush(0);
+                cntr = 0;
+            }
+            
+            if(cntr >= 3000)
+            {
+                cntr = 0;
+                currState = leader;
+            }
+            break;
+
+        default:
+            break;
     }
 }
-
-void receiveTick()
-{
-    if(USART_HasReceived(0))
-    {
-        tmpA = USART_Receive(0);
-        PORTA = tmpA;
-        USART_Flush(0);
-        
-    }
-    
-}
-
 
 int main(void) {
     /* Insert DDR and PORT initializations */
@@ -61,30 +102,29 @@ int main(void) {
     PORTA = 0x00;
     DDRC = 0xFF;
     PORTC = 0x00;
-    DDRB = 0x00;
-    PORTB = 0xFF;
+
     initUSART(0);
-    TimerSet(500);
+    initUSART(1);
+    TimerSet(1);
     TimerOn();
-    
+    USART_Flush(0);
+    currState = start;
     /* Insert your solution below */
     while (1)
     {
-        toggle = ~PINB;
-        while(!TimerFlag);
+        while(!TimerFlag)
+        {
+            
+        }
         TimerFlag = 0;
-        if (toggle == 0x01)
+        tickCount++;
+        cntr++;
+        if(tickCount == currCount)
         {
-            TimerSet(500);
-            PORTC = 0xFF;
-            sendTick();
+            Tick();
+            tickCount = 0;
         }
-        else
-        {
-            TimerSet(5);
-            PORTC = 0x00;
-            receiveTick();
-        }
+        PORTC = leadLED;
     }
     return 1;
 }
